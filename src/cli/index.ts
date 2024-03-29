92d06d8f13f78a4fae39352558ef66d54272d953
Maxime Fabas <fabas@lemonde.fr>
2024-03-29 16:51:05 +0100

    -

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -6,12 +6,10 @@ import path from 'node:path'
 const __filename = url.fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-console.log(__filename)
-console.log(__dirname)
-
 const firstFlag = process.argv.at(2)?.trim()
 if (firstFlag === '--list') {
-  console.log('I should list !')
-  // await fs.readFile()
-  // const commands = await fs.readFile()
+  const listFilePath = path.join(__dirname, 'assets/list.txt')
+  const list = await fs.readFile(listFilePath)
+  console.log(list)
+  process.exit(0)
 }
