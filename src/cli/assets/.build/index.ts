f8f964acdc44e17faed7770fd0d2dc3fa8e2122b
Maxime Fabas <fabas@lemonde.fr>
2024-03-29 16:48:41 +0100

    -

--- src/cli/assets/.build/index.ts
+++ src/cli/assets/.build/index.ts
@@ -1,20 +1,16 @@
 import process from 'node:process'
 import { promises as fs } from 'node:fs'
 import path from 'node:path'
+import url from 'node:url'
 
-const [_nodeBinPath, selfPath, assetsDirPath] = process.argv
-if (selfPath === undefined) {
-  console.error('Fatal: Self path not provided to builder script')
-  process.exit(1)
-}
+const [_nodePath, _scriptPath, assetsDirPath] = process.argv
+const __filename = url.fileURLToPath(import.meta.url)
 if (assetsDirPath === undefined) {
-  console.error('Fatal: Could not find the assets dir path from', selfPath)
+  console.error('Fatal: Could not find the assets dir path from', __filename)
   process.exit(1)
 }
 
 const buildFolderPath = path.join(assetsDirPath, '../..')
-const commands = (await fs.readdir(buildFolderPath))
-  .filter(e => e !== 'package.json')
-  .sort()
+const commands = (await fs.readdir(buildFolderPath)).filter(e => e !== 'package.json').sort()
 const outputPath = path.join(assetsDirPath, 'list.txt')
 await fs.writeFile(outputPath, commands.join('\n'), { encoding: 'utf-8' })
--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,10 +1,17 @@
 import process from 'node:process'
 import { promises as fs } from 'node:fs'
+import url from 'node:url'
+import path from 'node:path'
 
-console.log(process.argv)
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+console.log(__filename)
+console.log(__dirname)
 
 const firstFlag = process.argv.at(2)?.trim()
 if (firstFlag === '--list') {
   console.log('I should list !')
+  // await fs.readFile()
   // const commands = await fs.readFile()
 }
