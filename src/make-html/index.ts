ca9fdd7ad3064c63130ff330204a799f6ed06f3e
Maxime Fabas <fabas@lemonde.fr>
2024-03-29 19:02:17 +0100

    -

--- src/make-html/index.ts
+++ src/make-html/index.ts
@@ -8,6 +8,10 @@ const __filename = url.fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 const templatePath = path.join(__dirname, 'assets/template')
 
+console.log('-----')
+console.log('cp', templatePath, 'to', path.join(CWD, 'template'))
+console.log('-----')
+
 if (!existsSync(templatePath)) {
   console.error(`Could not find the template to copy at ${templatePath}`)
   process.exit(1)
