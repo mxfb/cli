51de472c897c16efd12476fc1f6cc2b05cf90797
Maxime Fabas <fabas@lemonde.fr>
2024-04-02 12:50:30 +0200

    -

--- src/cli/assets/.publish/index.ts
+++ src/cli/assets/.publish/index.ts
@@ -17,16 +17,19 @@ if (assetsDirPath === undefined) {
  * * * * * * * * * * * * * * * * * */
 
 const buildFolderPath = path.join(assetsDirPath, '../..')
-const commands = (await fs.readdir(buildFolderPath)).filter(e => e !== 'package.json').sort()
-const outputPath = path.join(assetsDirPath, 'list.txt')
-await fs.writeFile(outputPath, commands.join('\n'), { encoding: 'utf-8' })
+const commandsList = (await fs.readdir(buildFolderPath)).filter(e => e !== 'package.json').sort()
+const listFileOutputPath = path.join(assetsDirPath, 'list.txt')
+await fs.writeFile(listFileOutputPath, commandsList.join('\n'), { encoding: 'utf-8' })
 
 /* * * * * * * * * * * * * * * * * *
  *
  * Create cli/assets/version.txt
  *
  * * * * * * * * * * * * * * * * * */
-// const pkgJsonPath = path.join(assetsDirPath, '../../../package.json')
-// const pkgJsonContent = await fs.readFile(pkgJsonPath, { encoding: 'utf-8' })
-// const pkgJsonObj = JSON.parse(pkgJsonContent)
-// const { version } = pkgJsonObj
+
+const pkgJsonPath = path.join(assetsDirPath, '../../../package.json')
+const pkgJsonContent = await fs.readFile(pkgJsonPath, { encoding: 'utf-8' })
+const pkgJsonObj = JSON.parse(pkgJsonContent)
+const { version } = pkgJsonObj
+const versonFileOutputPath = path.join(assetsDirPath, 'version.txt')
+await fs.writeFile(versonFileOutputPath, `${version}`, { encoding: 'utf-8' })
