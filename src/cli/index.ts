9e065e87730f687f5c5e69c20ac618f46edf52eb
Maxime Fabas <fabas@lemonde.fr>
2024-04-02 13:13:18 +0200

    -

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -10,32 +10,34 @@ const __dirname = path.dirname(__filename)
 program
   .name('@mxfb-cli/cli')
   .description('The home of @mxfb/cli\'s project')
-  .option('-v, --version', 'print program\'s version')
+  .option('-l, --list', 'list all the available commands')
+  .option('-v, --version', 'print current version of the package')
   .action(async options => {
-    if (options.version === true) {
-      const versionFilePath = path.join(__dirname, 'assets/version.txt')
-      const version = await fs.readFile(versionFilePath, { encoding: 'utf-8' })
-      console.log(version)
-    }
+    if (options.list === true) listCommands()
+    else if (options.version === true) printVersion()
+    else program.help()
   })
   
 program
   .command('list')
   .description('list all the available commands')
-  .action(async () => {
-    const listFilePath = path.join(__dirname, 'assets/list.txt')
-    const list = await fs.readFile(listFilePath, { encoding: 'utf-8' })
-    console.log(list)
-  })
+  .action(listCommands)
 
 program
   .command('version')
   .description('print current version of the package')
-  .action(async () => {
-    const versionFilePath = path.join(__dirname, 'assets/version.txt')
-    const version = await fs.readFile(versionFilePath, { encoding: 'utf-8' })
-    console.log(version)
-  })
+  .action(printVersion)
 
 program.parse(process.argv)
 
+async function listCommands () {
+  const listFilePath = path.join(__dirname, 'assets/list.txt')
+  const list = await fs.readFile(listFilePath, { encoding: 'utf-8' })
+  console.log(list)
+}
+
+async function printVersion () {
+  const versionFilePath = path.join(__dirname, 'assets/version.txt')
+  const version = await fs.readFile(versionFilePath, { encoding: 'utf-8' })
+  console.log(version)
+}
