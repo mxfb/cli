20251904a495bfb2a57de14cf92ab131830045a8
Maxime Fabas <fabas@lemonde.fr>
2024-04-02 12:56:46 +0200

    -

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -11,8 +11,12 @@ program
   .name('@mxfb-cli/cli')
   .description('The home of @mxfb/cli\'s project')
   .option('-v, --version', 'print program\'s version')
-  .action((...args) => {
-    console.log(args)
+  .action(async options => {
+    if (options.version === true) {
+      const versionFilePath = path.join(__dirname, 'assets/version.txt')
+      const version = await fs.readFile(versionFilePath, { encoding: 'utf-8' })
+      console.log(version)
+    }
   })
   
 program
@@ -22,6 +26,15 @@ program
     const listFilePath = path.join(__dirname, 'assets/list.txt')
     const list = await fs.readFile(listFilePath, { encoding: 'utf-8' })
     console.log(list)
+  })
+
+program
+  .command('version')
+  .description('print current version of the package')
+  .action(async () => {
+    const versionFilePath = path.join(__dirname, 'assets/version.txt')
+    const version = await fs.readFile(versionFilePath, { encoding: 'utf-8' })
+    console.log(version)
   })
 
 program.parse(process.argv)
