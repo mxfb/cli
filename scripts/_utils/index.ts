231acd0a2e91b50182633cba1a75f3a8eb029e4b
Maxime Fabas <maxime.fabas@pm.me>
2024-04-08 00:19:07 +0200

    ~/scripts/_utils - use @design-edito/tools - listSubpaths

--- scripts/_utils/index.ts
+++ scripts/_utils/index.ts
@@ -1,33 +1,21 @@
-import { promises as fs, existsSync } from 'node:fs'
 import path from 'node:path'
 
+import listSubpaths from '@design-edito/tools/utils/node/list-subpaths'
+
 export async function listSubdirectoriesIndexes (root: string, extensions?: string[]): Promise<string[]> {
-  if (!existsSync(root)) return []
-  const rootStat = await fs.stat(root)
-  const rootIsDir = rootStat.isDirectory()
-  if (!rootIsDir) return []
-  const rootChildren = (await fs.readdir(root)).map(name => path.join(root, name))
-  const subdirectoriesIndexes = (await Promise.all(rootChildren.map(async filePath => {
-    try {
-      if (!existsSync(filePath)) return false
-      const stat = await fs.stat(filePath)
-      const isDirectory = stat.isDirectory()
-      if (!isDirectory) return undefined
-      const children = await fs.readdir(filePath)
-      const foundIndex = children.find(childName => {
-        const extension = path.extname(childName).toLowerCase()
-        const baseName = path.basename(childName, extension)
-        const extensionIsValid = extensions !== undefined ? extensions.includes(extension) : true
-        if (baseName === 'index' && extensionIsValid) return true
-        return false
-      })
-      if (foundIndex === undefined) return false
-      return path.join(filePath, foundIndex)
-    } catch (err) {
-      return undefined
+  return await listSubpaths(root, {
+    directories: false,
+    symlinks: false,
+    hidden: false,
+    maxDepth: 1,
+    filter: (filePath, { type }) => {
+      if (type !== 'file') return false
+      const extension = path.extname(filePath)
+      const basename = path.basename(filePath, extension)
+      const extensionIsValid = extensions?.includes(extension) ?? true
+      return basename === 'index' && extensionIsValid
     }
-  }))).filter((e): e is string => e !== undefined)
-  return subdirectoriesIndexes
+  })
 }
 
 export function isInDirectory (childPath: string, parentPath: string) {
