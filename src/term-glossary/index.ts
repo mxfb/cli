077fac34b3244ef11c9a7b4048a5ebb18baea77c
Maxime Fabas <maxime.fabas@pm.me>
2024-04-15 14:55:39 +0200

    src/term-glossary - update

--- src/term-glossary/index.ts
+++ src/term-glossary/index.ts
@@ -4,16 +4,20 @@ console.log(`: <<'END'
 
 1. git & GitHub
 
-  1.1 Comparing
+  1.1 Forking
 
-    git fetch upstream # récup upstream à jour
-    git status # Affiche le diff avec upstream/master
-    git log HEAD..upstream/master # log les commits de upstream/master
+    1.1.1 Creating the fork
+    
+    1.1.2 Comparing
 
-  1.2 Updating
+      git fetch upstream                # récup upstream à jour
+      git status                        # Affiche le diff avec upstream/master
+      git log HEAD..upstream/master     # log les commits de upstream/master
 
-    git merge upstream/master 
-    git push origin master
+    1.1.3 Updating
+
+      git merge upstream/master         # récupère upstream/master dans origin/master
+      git push origin master
 
 
 END
