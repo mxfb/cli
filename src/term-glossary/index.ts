309328ef33b150f24b7516cb61e733064188b2b9
Maxime Fabas <maxime.fabas@pm.me>
2024-04-15 14:49:37 +0200

    src/term-glossary - created

---
+++ src/term-glossary/index.ts
@@ -0,0 +1,21 @@
+console.log(`: <<'END'
+
+‹ TERM GLOSSARY ›
+
+1. git & GitHub
+
+  1.1 Comparing
+
+    git fetch upstream # récup upstream à jour
+    git status # Affiche le diff avec upstream/master
+    git log HEAD..upstream/master # log les commits de upstream/master
+
+  1.2 Updating
+
+    git merge upstream/master 
+    git push origin master
+
+
+END
+
+`)
