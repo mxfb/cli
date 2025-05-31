437e2e7e3a76378fc9feaae6c348f25c37e3a1c2
Maxime Fabas <maxime.fabas@pm.me>
2025-05-31 19:46:12 +0200

    -

--- src/add/index.ts
+++ src/add/index.ts
@@ -3,11 +3,11 @@ import { program } from 'commander'
 
 program
   .name('@design-edito/add')
-  .description('Shorthand for git add with arguments forwarding (defaults to "*")')
+  .description('Shorthand for git add with arguments forwarding (defaults to ".")')
   .allowUnknownOption(true)
   .arguments('[args...]')
   .action((args: string[]) => {
-    const finalArgs = args.length > 0 ? args.join(' ') : '.*'
+    const finalArgs = args.length > 0 ? args.join(' ') : '.'
     exec(`git add ${finalArgs}`, (error, stdout, stderr) => {
       if (error) console.log('\nERR\n', error)
       if (stderr) console.log('\nstderr:\n', stderr)
